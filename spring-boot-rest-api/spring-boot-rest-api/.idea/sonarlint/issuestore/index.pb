17837fbf7c98b52c6569d0453a9de803f0231143
Guguloth Vamshi Naik <guguloth.vamshi@ria.insure>
2022-09-21 09:30:33 +0530

    Query string for search filter

---
+++ spring-boot-rest-api/spring-boot-rest-api/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+€
+Psrc/main/java/com/springboot/springbootrestapi/SpringBootRestApiApplication.java,1\4\14c96bcf2e20a25ff6c19d9c62d403851cffcc96
+„
+Tsrc/main/java/com/springboot/springbootrestapi/exception/TrailNotFoundException.java,4\f\4fe87d7db50aa5b8c8ebf2b6862fe7fe11d699c5
+~
+Nsrc/main/java/com/springboot/springbootrestapi/validations/NameValidation.java,9\2\92fea83ab474eac86b246e581502930d0972cac0
+{
+Ksrc/test/java/com/springboot/springbootrestapi/TrekBookingServiceTests.java,7\3\7383357b0fe72cf57cf8538cad8a20ff3349dc01
--- spring-boot-rest-api/spring-boot-rest-api/src/main/java/com/springboot/springbootrestapi/repository/TrailRepository.java
+++ spring-boot-rest-api/spring-boot-rest-api/src/main/java/com/springboot/springbootrestapi/repository/TrailRepository.java
@@ -1,12 +1,15 @@
 package com.springboot.springbootrestapi.repository;
 
 import com.springboot.springbootrestapi.model.Trail;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TrailRepository extends MongoRepository <Trail, String> {
+    @Query("{ 'name': { '$regex':'?0', '$options':'i' }}")
     List<Trail> findTrailByName(String name);
 }
